5fbd01455cb326b91075f59950ad264138082be3
vulcan <vulcan.lee@gmail.com>
2020-06-06 16:57:44 +0800

    頁面導航(透過導航頁面) Prism

---
+++ XF2003/XF2003/XF2003/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF2003.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly INavigationService navigationService;
+        public DelegateCommand GoNextCommand { get; set; }
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            GoNextCommand = new DelegateCommand(OnGoNextCommand);
+        }
+
+        private void OnGoNextCommand()
+        {
+            navigationService.NavigateAsync("NextPage");
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
---
+++ XF2003/XF2003/XF2003/ViewModels/NextPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XF2003.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class NextPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly INavigationService navigationService;
+        public DelegateCommand GoBackCommand { get; set; }
+        public NextPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            GoBackCommand = new DelegateCommand(OnGoBackCommand);
+        }
+
+        private void OnGoBackCommand()
+        {
+            navigationService.GoBackAsync();
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
