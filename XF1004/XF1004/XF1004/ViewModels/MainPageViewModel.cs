8e8ad85c0ce53c85b81880be25506ac8e8582d70
vulcan <vulcan.lee@gmail.com>
2020-06-06 12:11:25 +0800

    Xamarin.Forms for Prism 專案練習 使用Prism+MVVM開發方法

---
+++ XF1004/XF1004/XF1004/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF1004.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly INavigationService navigationService;
+        public string YourName { get; set; }
+        public string HelloBody { get; set; }
+        public DelegateCommand SendCommand { get; set; }
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            SendCommand = new DelegateCommand(OnSendCommand);
+        }
+
+        private void OnSendCommand()
+        {
+            HelloBody = YourName + " 你好!!";
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
