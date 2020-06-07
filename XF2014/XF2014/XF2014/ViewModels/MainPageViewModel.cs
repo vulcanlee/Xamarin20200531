445c658bb1a50ed9cb76472a456bb1e29a4fcb8a
vulcan <vulcan.lee@gmail.com>
2020-06-07 16:55:34 +0800

    XAML 事件 命令之行為

---
+++ XF2014/XF2014/XF2014/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF2014.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly INavigationService navigationService;
+        private readonly IPageDialogService dialogService;
+
+        public DelegateCommand BtnClickedCommand { get; set; }
+        public MainPageViewModel(INavigationService navigationService,
+            IPageDialogService dialogService)
+        {
+            this.navigationService = navigationService;
+            this.dialogService = dialogService;
+
+            BtnClickedCommand = new DelegateCommand(OnBtnClickedCommand);
+        }
+
+        private void OnBtnClickedCommand()
+        {
+            dialogService.DisplayAlertAsync("普天同慶", "恭喜你，抽中19億元大獎",
+                "收到");
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
