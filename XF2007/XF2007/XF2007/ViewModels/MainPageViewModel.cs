b3e0be285cc1eed103a76a37cf650fba66313afe
vulcan <vulcan.lee@gmail.com>
2020-06-07 11:37:10 +0800

    頁面導航且傳遞引數

---
+++ XF2007/XF2007/XF2007/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF2007.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly INavigationService navigationService;
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+        public string FromNextPageMessage { get; set; }
+        public DelegateCommand AddCommand { get; set; }
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            AddCommand = new DelegateCommand(OnAddCommand);
+        }
+
+        private void OnAddCommand()
+        {
+            var para = new NavigationParameters();
+            para.Add("V1", Value1);
+            para.Add("V2", Value2);
+            navigationService.NavigateAsync("NextPage", para);
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            var msg = parameters.GetValue<string>("Response");
+            FromNextPageMessage = msg;
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
---
+++ XF2007/XF2007/XF2007/ViewModels/NextPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XF2007.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class NextPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly INavigationService navigationService;
+        public int Sum { get; set; }
+        public string Response { get; set; }
+        public DelegateCommand GobackCommand { get; set; }
+        public NextPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            GobackCommand = new DelegateCommand(OnGobackCommand);
+        }
+
+        private void OnGobackCommand()
+        {
+            var para = new NavigationParameters();
+            para.Add("Response", Response);
+            navigationService.GoBackAsync(para);
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            int v1 = parameters.GetValue<int>("V1");
+            int v2 = parameters.GetValue<int>("V2");
+            Sum = v1 + v2;
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
