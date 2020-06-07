56dd67fd5e4002b788e5936ba0346a1325f64232
vulcan <vulcan.lee@gmail.com>
2020-06-07 12:19:16 +0800

    Prism選擇項目之對話窗

---
+++ XF2012/XF2012/XF2012/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XF2012.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly INavigationService navigationService;
+        private readonly IPageDialogService dialogService;
+
+        public string YourChoice { get; set; }
+        public DelegateCommand EatCommand { get; set; }
+        public MainPageViewModel(INavigationService navigationService,
+            IPageDialogService dialogService)
+        {
+            this.navigationService = navigationService;
+            this.dialogService = dialogService;
+            EatCommand = new DelegateCommand(OnEatCommand);
+        }
+
+        private async void OnEatCommand()
+        {
+            YourChoice=await dialogService.DisplayActionSheetAsync("要吃甚麼?",
+                "取消", null,"蘋果", "香蕉", "荔枝");
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+        }
+
+    }
+}
